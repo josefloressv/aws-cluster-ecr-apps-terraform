98ed035343b13edb22bc4307da1ee027a0546e3f
José Flores <josefloressv@gmail.com>
2022-04-20 21:26:18 -0600

    IaC ECR repo definition

---
+++ main.tf
@@ -0,0 +1,44 @@
+terraform {
+    required_version = ">= 1.1.9"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = ">= 4.10.0"
+        }
+    }
+
+    backend "s3" {}
+}
+
+resource "aws_ecr_repository" "ecr-app" {
+  name                 = var.ecr_repo_name
+  image_tag_mutability = "IMMUTABLE"
+  tags = merge(
+      local.tags,
+      var.ecr_repo_tags
+  )
+}
+
+resource "aws_ecr_lifecycle_policy" "ecr-app-policy" {
+  repository = aws_ecr_repository.ecr-app.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "${var.ecr_repo_policy_description}",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": ${var.ecr_repo_policy_expiration_days}
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+# General
+variable "base_tags" {
+    type = map(string)
+    description = "The base tags for the stack"
+}
+
+# ECR
+variable "ecr_repo_name" {
+    type = string
+    description = "The name of the ECR repository"
+}
+
+variable "ecr_repo_tags" {
+    type = map(string)
+    description = "The tags to apply to the ECR repository"
+}
+
+variable "ecr_repo_policy_expiration_days" {
+    type = number
+    description = "The expiration date for the ECR repository policy"
+}
+
+variable "ecr_repo_policy_description" {
+    type = string
+    description = "The description for the ECR repository policy"
+}
